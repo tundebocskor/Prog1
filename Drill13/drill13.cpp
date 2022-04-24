1d675740d04d120f7a78b2aa74b80214b4897b75
tundebocskor <90981751+tundebocskor@users.noreply.github.com>
2022-04-24 17:49:37 +0200

    Update drill13.cpp

--- Drill13/drill13.cpp
+++ Drill13/drill13.cpp
@@ -11,8 +11,8 @@ try {
     // Drill 13
 
     // 1. Hozzon létre egy 800x800 es ablakot
-    const Point tl {100, 100};
-    Simple_window win {tl, 800, 800, "Drill 13"};
+  
+    Simple_window win {Point {100, 100}, 800, 800, "Drill 13"};
     win.wait_for_button();
 
     // 2. Készíts 8x8 as, 100X100 pixeles négyzetrácsos ablakot
@@ -34,15 +34,19 @@ try {
     win.wait_for_button();
 
     // 3. Átlósan piros négyzetek
-    Vector_ref<Rectangle> rects;
-    for (int i = 0; i < x_size; i += x_grid) {
-        rects.push_back(new Rectangle{Point{i,i}, Point{i+x_grid,i+x_grid}});
-        rects[rects.size() - 1].set_color(Color::invisible);
-        rects[rects.size() - 1].set_fill_color(Color::red);
-        win.attach(rects[rects.size() - 1]);
-    }
+    
+    Vector_ref<Rectangle> vr;
+    //Névtelen objektumok létrehozásához szükséges. 
+        for(int i=0;i<8;i++)
+        {
+            //Létrehozza ezeket a névtelen objektumokat
 
-    win.wait_for_button();
+            vr.push_back(new Rectangle(Point{100*i,100*i},100,100));
+            vr[vr.size()-1].set_fill_color(Color::red);
+            win.attach(vr[vr.size()-1]);
+        }
+        win.wait_for_button();
+
 
     // 4. Adjon hozzá egy 200x200 as képet és helyezze el 3 helyen
     Image madar1 {Point{0,400}, "image.jpeg"};
@@ -55,16 +59,25 @@ try {
     win.wait_for_button();
 
     // 5. Adjon hozzá egy 100x100 as képet és mozogjon ha a "Nextet" megnyomjuk
-    Image Cica {Point{0,0}, "cica.gif"};
-    win.attach(Cica);
+    Image kep {Point{0,0}, "kep.jpg"};
+    win.attach(kep);
     win.wait_for_button();
+    int u=0;
+    int veg=7;
 
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            Cica.move(100, 0);
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < veg; ++j) {
+            
+            kep.move(100, 0);
+            u=j;
+
+
             win.wait_for_button();
+
         }   
-        Cica.move(-700, 100);          // buggy
+        veg=veg-1;
+        u=u*(-100);
+        kep.move(u, 100);         
         win.wait_for_button();
     }
 }
